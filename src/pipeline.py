2fcae02230e125a6465c99cb26b8421434e5907d
aneeshcheriank <aneeshcheriank@gmail.com>
2026-08-05 22:23:43 +0200

    - exposed number fo chunks to downstream - typo corrected module (import change)

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,6 @@
 from langchain_core.output_parsers import StrOutputParser
 
-from src.retriver import get_vectorstore
+from src.retriever import get_vectorstore
 from src.model import get_llm
 from src.prompt import rag_prompt
 
@@ -12,9 +12,9 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-def rag(question, chat_history=[]):
+def rag(question, chat_history=[], k=4):
     llm = get_llm()
-    vectorstore = get_vectorstore(k=4)
+    vectorstore = get_vectorstore(k=k)
     prompt = rag_prompt
 
     chain = prompt | llm | StrOutputParser()
