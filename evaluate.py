215dc15bf357a27887a1081399908ae7fec6cfcf
aneeshcheriank <aneeshcheriank@gmail.com>
2026-08-14 09:44:46 +0200

    formating

--- evaluate.py
+++ evaluate.py
@@ -160,10 +160,8 @@ def build_evaluator_llm():
     patched = instructor.from_openai(client, mode=instructor.Mode.JSON)
 
     # Neuter Ragas' broken patcher so llm_factory doesn't undo our fix.
-     # pylint: disable-next=protected-access
-    _ragas_base._patch_client_for_provider = (
-        lambda c, p: patched
-    ) 
+    # pylint: disable-next=protected-access
+    _ragas_base._patch_client_for_provider = lambda c, p: patched
 
     return llm_factory(
         config.LLM_MODEL,
